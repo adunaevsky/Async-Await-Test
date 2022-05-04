0e8e066208d3e804bccf1b16fa5795cc856fb4d2
Alex Dunaevsky <Alex.Dunaevsky@oahpp.ca>
2022-05-04 13:58:24 -0400

    added jest testing

---
+++ app.cjs
@@ -0,0 +1,32 @@
+const axios = require('axios');
+
+async function getMovieList(year) {
+
+    var url = "https://jsonmock.hackerrank.com/api/movies/";
+
+    var result = await axios.get(url, {
+        params: {
+            Year: year
+        }
+    });
+
+    return result.data.data.length > 0 ? result.data.data.map((d) => { return d.Title }) : 'No results found';
+}
+
+
+async function main() {
+    var year = 2018;
+   return await getMovieList(year);
+
+}
+
+main();
+
+function sum(a, b) {
+    return a + b;
+}
+module.exports = {
+    sum: sum,
+    main: main,
+    getMovieList: getMovieList
+};
